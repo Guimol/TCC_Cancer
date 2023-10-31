bb6ccdfa69c67d40c8cdd6563795b84c61491ced
Guimol <guizera11@hotmail.com.br>
2023-10-31 15:20:22 -0300

    improved readability

--- train.py
+++ train.py
@@ -3,8 +3,10 @@ import comet_ml
 
 print("Imported comet_ml")
 
-model_name = "resnet"
-training_mode = "transfer_learning"
+model_name = "resnet" # models: ["resnet", "efficientnet", "densenet", "transformer"]
+training_mode = "transfer_learning" # modes: ["transfer_learning", "fine_tuning", "from_scratch"]
+weight_path = "/home/guilherme/tcc_guilherme/densenet_no_preprocessing_transfer_learning_v1/checkpoints/ckpt_" # only used if training_mode == "fine_tuning"
+saved_model_path = "/home/guilherme/tcc_guilherme/densenet_no_preprocessing_transfer_learning_v0/model" # only used if training_mode == "fine_tuning"
 
 experiment_name = f'{model_name}_no_preprocessing_{training_mode}_v'
 
@@ -30,9 +32,8 @@ experiment = comet_ml.Experiment(
 print("Set experiment params")
 
 from tensorflow import keras
-from keras.applications import ResNet50, DenseNet121, EfficientNetB0
 
-print("Imported TF and models")
+print("Imported keras lib")
 
 # Define a function for decaying the learning rate.
 # You can define any decay function you need.
@@ -51,7 +52,7 @@ params = {
   "batch_size": 16,
   "epochs": 200,
   "input_size": (224, 224, 3),
-  "learning_rate": 0.001,
+  "learning_rate": 1e-3,
   "lr_scheduler": decay
 }
 
@@ -108,7 +109,13 @@ print("Set the strategy to mirrored")
 from model import get_model
 
 with mirrored_strategy.scope():
-  custom_model = get_model("resnet", params, "transfer_learning", None)
+  custom_model = get_model(
+                    base_model_name=model_name, 
+                    params=params, 
+                    training_mode=training_mode, 
+                    weight_path=weight_path,
+                    saved_model_path=saved_model_path
+                  )
 
 # Define the checkpoint directory to store the checkpoints.
 checkpoint_dir = os.path.join(os.getcwd(), experiment_name, "checkpoints")
@@ -119,7 +126,7 @@ print("Set the save dir for training")
 
 # Put all the callbacks together.
 callbacks = [
-    keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=20, verbose=1, mode='auto'),
+    keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=1e-3, patience=20, verbose=1, mode='auto'),
     keras.callbacks.ModelCheckpoint(filepath=checkpoint_prefix,
                                        save_weights_only=True,
                                        save_best_only=True),
